5b14ca867ee542c70130ce1b02b2aaae1858b661
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-10 09:49:28 +0200

    Updates classes names to CamelCase

--- src/domini/classes/CodeBreak.java
+++ src/domini/classes/CodeBreak.java
@@ -2,6 +2,6 @@ package domini.classes;
 
 import domini.classes.Action;
 
-public class Codebreak extends Action {
+public class CodeBreak extends Action {
     public java.util.Collection turn = new java.util.TreeSet();
 }
--- src/domini/classes/CodeMake.java
+++ src/domini/classes/CodeMake.java
@@ -2,6 +2,6 @@ package domini.classes;
 
 import domini.classes.Action;
 
-public class Codemake extends Action {
+public class CodeMake extends Action {
     public java.util.Collection code = new java.util.TreeSet();
 }
