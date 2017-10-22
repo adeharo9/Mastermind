4d888371c7b2300fa9d8e1112f8946706d7a1883
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-23 00:22:50 +0200

    Adds Pair default constructor

--- src/util/Pair.java
+++ src/util/Pair.java
@@ -5,6 +5,12 @@ public class Pair<T1, T2>
     public T1 first;
     public T2 second;
 
+    public Pair()
+    {
+        this.first = null;
+        this.second = null;
+    }
+
     public Pair(T1 first, T2 second)
     {
         this.first = first;
@@ -14,5 +20,10 @@ public class Pair<T1, T2>
     public boolean hasNull()
     {
         return first == null || second == null;
+    }
+
+    public static <T1, T2> boolean isValid(Pair<T1, T2> pair)
+    {
+        return pair != null && !pair.hasNull();
     }
 }
