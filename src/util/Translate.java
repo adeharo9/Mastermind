e381140811d2eba147f7de7a30d48d2fee466569
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-23 23:29:52 +0200

    Adds inverse translation methods

--- src/util/Translate.java
+++ src/util/Translate.java
@@ -2,6 +2,8 @@ package util;
 
 public abstract class Translate
 {
+    /* DIFFICULTY METHODS */
+
     public static Difficulty int2Difficulty(int diff)
     {
         Difficulty difficulty;
@@ -25,6 +27,31 @@ public abstract class Translate
         return difficulty;
     }
 
+    /* ROLE METHODS */
+
+    public static int difficulty2Int(Difficulty difficulty)
+    {
+        int diff;
+
+        switch(difficulty)
+        {
+            case easy:
+                diff = 0;
+                break;
+            case medium:
+                diff = 1;
+                break;
+            case hard:
+                diff = 2;
+                break;
+            default:
+                diff = -1;
+                break;
+        }
+
+        return diff;
+    }
+
     public static Role int2Role(int rol)
     {
         Role role;
@@ -46,5 +73,28 @@ public abstract class Translate
         }
 
         return role;
+    }
+
+    public static int role2Int(Role role)
+    {
+        int rol;
+
+        switch(role)
+        {
+            case codeMaker:
+                rol = 0;
+                break;
+            case codeBreaker:
+                rol = 1;
+                break;
+            case watcher:
+                rol = 2;
+                break;
+            default:
+                rol = -1;
+                break;
+        }
+
+        return rol;
     }
 }
