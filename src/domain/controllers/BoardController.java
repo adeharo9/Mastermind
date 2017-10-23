613ec9a8f14feba92bcbbfe8510aed8332999a39
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-23 18:54:25 +0200

    Adds private functions

--- src/domain/controllers/BoardController.java
+++ src/domain/controllers/BoardController.java
@@ -7,12 +7,51 @@ import java.util.ArrayList;
 
 public class BoardController
 {
+    /* ATTRIBUTES */
+
     private Board board;
+
+    /* PRIVATE METHODS */
+
+    private static Difficulty getDifficultyByInt(int diff)
+    {
+        Difficulty difficulty;
+
+        switch(diff)
+        {
+            case 0:
+                difficulty = Difficulty.easy;
+                break;
+            case 1:
+                difficulty = Difficulty.medium;
+                break;
+            case 2:
+                difficulty = Difficulty.hard;
+                break;
+            default:
+                difficulty = null;
+                break;
+        }
+
+        return difficulty;
+    }
+
+    /* CONSTRUCTION METHODS */
 
     public BoardController()
     {
         board = new Board();
     }
+
+    /* INITIALIZATION METHODS */
+
+    public void newBoard(int diff) throws IllegalArgumentException, NullPointerException
+    {
+        Difficulty difficulty = getDifficultyByInt(diff);
+        board = new Board(difficulty);
+    }
+
+    /* SET METHODS */
 
     public boolean setTurn()
     {
@@ -21,15 +60,10 @@ public class BoardController
         return true;
     }
 
-    public boolean newBoard(Difficulty difficulty) throws Exception
+    /* GET METHODS */
+
+    public Board getBoard()
     {
-        board = new Board();
-
-        board.setNColumns(Board.getNColumnsByDifficulty(difficulty));
-        board.setMaxAttempts(Board.getMaxAttemptsByDifficulty(difficulty));
-
-        board.setTurnSet(new ArrayList<>());
-
-        return board.isValid();
+        return board;
     }
 }
