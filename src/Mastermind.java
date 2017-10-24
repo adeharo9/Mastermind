1c5383f77b0092abe5920f4bbb4a9c577dca8542
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-24 22:09:18 +0200

    Throws non-handled exceptions outside the program

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,10 +1,11 @@
 import domain.controllers.DomainController;
+import exceptions.IntegrityCorruption;
 import presentation.controllers.PresentationController;
 import testing.MainTestController;
 
 public class Mastermind
 {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception{
         if (args.length != 0)
         {
             switch (args[0]) {
