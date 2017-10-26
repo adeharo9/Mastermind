0cb486475791c7bdc808f6a27fce35c731c72537
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-26 23:57:03 +0200

    Adds ioUtils to testdriver

--- src/testing/drivers/TestPersistence.java
+++ src/testing/drivers/TestPersistence.java
@@ -2,26 +2,37 @@ package testing.drivers;
 import domain.classes.Game;
 import persistence.GamePersistence;
 import util.Difficulty;
+import util.Utils;
+import util.ioUtils;
 
-public class TestPersistence {
-    public static void main(String args[]) {
-        Game test = new Game(10, Difficulty.easy);
+public class TestPersistence
+{
+    private static final int gameID = Utils.autoIDInt();
+
+    public static void main(String args[])
+    {
+        Game test = new Game(gameID, Difficulty.easy);
         Game result = new Game();
-        GamePersistence bd = new GamePersistence();
-        try{
-            bd.save(test);
+        GamePersistence gP = new GamePersistence();
+
+        try
+        {
+            gP.save(test);
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
 
-        try{
-            result = bd.load(Integer.toString(10));
+        try
+        {
+            result = gP.load(Integer.toString(gameID));
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
-        System.out.println(Integer.toString(result.getId()));
+        ioUtils.printOutLn(Integer.toString(result.getId()));
     }
 }
 
