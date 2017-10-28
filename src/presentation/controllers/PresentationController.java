e5d4f48b8ec9d0d0479638f4378777ed82143dbc
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-28 21:50:38 +0200

    Updates menus

--- src/presentation/controllers/PresentationController.java
+++ src/presentation/controllers/PresentationController.java
@@ -9,14 +9,14 @@ public class PresentationController
 
     /* PRIVATE METHODS */
 
-    private Pair<String, String> getInfoUser() throws NumberFormatException
+    private Pair<String, String> getInfoUser()
     {
         ioUtils.endLine();
-        ioUtils.printOutLn ("Write your username: ");
+        ioUtils.printOutLn("Write your username: ");
         String username = ioUtils.input();
 
         ioUtils.endLine();
-        ioUtils.printOutLn ("Write your password: ");
+        ioUtils.printOutLn("Write your password: ");
         String password = ioUtils.input();
 
         return new Pair<>(username, password);
@@ -34,7 +34,7 @@ public class PresentationController
     public void wrongOption()
     {
         ioUtils.endLine();
-        ioUtils.printOutLn("Wrong option number.");
+        ioUtils.printOutLn("Wrong option number");
         ioUtils.endLine();
     }
 
@@ -52,98 +52,14 @@ public class PresentationController
         ioUtils.endLine();
     }
 
-    public int initialMenu () throws NumberFormatException
+    public int initSessionMenu() throws NumberFormatException
     {
-        // Menu inicial: iniciar sesion/registrar jugador
         ioUtils.printOutLn ("1.- Register user");
         ioUtils.printOutLn ("2.- Log in");
         ioUtils.printOutLn ("0.- Close");
         ioUtils.endLine();
-        ioUtils.printOut("Select an option: ");
+        ioUtils.printOut("Select option number: ");
         return Integer.parseInt(ioUtils.input());
-    }
-
-    public int gameSelectionMenu() throws NumberFormatException
-    {
-        // Menú de seleccion: cargar partida, partida nueva, etc.
-        ioUtils.printOutLn ("1.- New game");
-        ioUtils.printOutLn ("2.- Load game");
-        ioUtils.printOutLn ("3.- Ranking");
-        ioUtils.printOutLn ("4.- Info");
-        ioUtils.printOutLn ("0.- Log out");
-        ioUtils.endLine();
-        ioUtils.printOut("Select an option: ");
-        return Integer.parseInt(ioUtils.input());
-    }
-
-    public int gameModeSelectionMenu() throws NumberFormatException
-    {
-        // Menú de selección de modo de juego: codebreaker, codemaker, etc.
-        ioUtils.endLine();
-        ioUtils.printOutLn ("1.- Codemaker");
-        ioUtils.printOutLn ("2.- Codebreaker");
-        //ioUtils.printOutLn ("3.- Watch");
-        ioUtils.printOutLn ("0.- Back");
-        ioUtils.endLine();
-        ioUtils.printOut("Select game mode: ");
-        return Integer.parseInt(ioUtils.input());
-    }
-
-    public int gameDifficultySelectionMenu() throws NumberFormatException
-    {
-        // Menú de selección de dificultad de juego: fácil, medio, difícil, etc.
-        ioUtils.endLine();
-        ioUtils.printOutLn ("1.- Easy");
-        ioUtils.printOutLn ("2.- Medium");
-        ioUtils.printOutLn ("3.- Hard");
-        ioUtils.printOutLn ("0.- Back");
-        ioUtils.endLine();
-        ioUtils.printOut("Select difficulty: ");
-        return Integer.parseInt(ioUtils.input());
-    }
-
-    public int inGameMenu() throws NumberFormatException
-    {
-        // Menu de seleccion de accion en el juego: tirar turno o bien pausa
-        ioUtils.endLine();
-        ioUtils.printOutLn ("1.- Write your code");
-        ioUtils.printOutLn ("0.- Pause game");
-        ioUtils.endLine();
-        ioUtils.printOut("Select an option: ");
-        return Integer.parseInt(ioUtils.input());
-    }
-
-    public int pauseMenu() throws NumberFormatException
-    {
-        // Menu de pausa del juego: guardar partida, salir, etc
-        ioUtils.endLine();
-        ioUtils.printOutLn ("1.- Save and continue");
-        ioUtils.printOutLn ("2.- Save and leave");
-        ioUtils.printOutLn ("3.- Leave without saving");
-        ioUtils.printOutLn ("4.- Ask for clue");
-        ioUtils.printOutLn ("0.- Continue game");
-        ioUtils.endLine();
-        ioUtils.printOut("Select an option: ");
-        return Integer.parseInt(ioUtils.input());
-    }
-
-    public int savegame() throws NumberFormatException
-    {
-        ioUtils.endLine();
-        ioUtils.printOutLn ("Do you want to change the name of the game?");
-        ioUtils.endLine();
-        ioUtils.printOutLn ("1.- Yes");
-        ioUtils.printOutLn ("0.- No");
-        ioUtils.endLine();
-        ioUtils.printOut("Select an option: ");
-        return Integer.parseInt(ioUtils.input());
-    }
-
-    public String setGameName()
-    {
-        ioUtils.endLine();
-        ioUtils.printOutLn ("Enter a name for the game:");
-        return ioUtils.input();
     }
 
     public Pair<String, String> logInMenu() throws NumberFormatException
@@ -154,6 +70,95 @@ public class PresentationController
     public Pair<String, String> registerUserMenu() throws NumberFormatException
     {
         return getInfoUser();
+    }
+
+    public int mainGameMenu() throws NumberFormatException
+    {
+        ioUtils.printOutLn ("1.- New game");
+        ioUtils.printOutLn ("2.- Load game");
+        ioUtils.printOutLn ("3.- Ranking");
+        ioUtils.printOutLn ("4.- Info");
+        ioUtils.printOutLn ("0.- Log out");
+        ioUtils.endLine();
+        ioUtils.printOut("Select option number: ");
+        return Integer.parseInt(ioUtils.input());
+    }
+
+    public int gameModeSelectionMenu() throws NumberFormatException
+    {
+        ioUtils.endLine();
+        ioUtils.printOutLn ("1.- Human vs. Human");
+        ioUtils.printOutLn ("2.- Human vs. CPU");
+        ioUtils.printOutLn ("3.- CPU vs. CPU");
+        ioUtils.printOutLn ("0.- Back");
+        ioUtils.endLine();
+        ioUtils.printOut("Select option number: ");
+        return Integer.parseInt(ioUtils.input());
+    }
+
+    public int gameRoleSelectionMenu() throws NumberFormatException
+    {
+        ioUtils.endLine();
+        ioUtils.printOutLn ("1.- Code Maker");
+        ioUtils.printOutLn ("2.- Code Breaker");
+        ioUtils.printOutLn ("0.- Back");
+        ioUtils.endLine();
+        ioUtils.printOut("Select option number: ");
+        return Integer.parseInt(ioUtils.input());
+    }
+
+    public int gameDifficultySelectionMenu() throws NumberFormatException
+    {
+        ioUtils.endLine();
+        ioUtils.printOutLn ("1.- Easy");
+        ioUtils.printOutLn ("2.- Medium");
+        ioUtils.printOutLn ("3.- Hard");
+        ioUtils.printOutLn ("0.- Back");
+        ioUtils.endLine();
+        ioUtils.printOut("Select option number: ");
+        return Integer.parseInt(ioUtils.input());
+    }
+
+    public int inGameMenu() throws NumberFormatException
+    {
+        ioUtils.endLine();
+        ioUtils.printOutLn ("1.- Write your code");
+        ioUtils.printOutLn ("0.- Pause game");
+        ioUtils.endLine();
+        ioUtils.printOut("Select option number: ");
+        return Integer.parseInt(ioUtils.input());
+    }
+
+    public int pauseMenu() throws NumberFormatException
+    {
+        ioUtils.endLine();
+        ioUtils.printOutLn ("1.- Save and continue");
+        ioUtils.printOutLn ("2.- Save and exit");
+        ioUtils.printOutLn ("3.- Exit without saving");
+        ioUtils.printOutLn ("4.- Ask for clue");
+        ioUtils.printOutLn ("0.- Continue game");
+        ioUtils.endLine();
+        ioUtils.printOut("Select option number: ");
+        return Integer.parseInt(ioUtils.input());
+    }
+
+    public int saveGame() throws NumberFormatException
+    {
+        ioUtils.endLine();
+        ioUtils.printOutLn ("Do you want to change the name of the game?");
+        ioUtils.endLine();
+        ioUtils.printOutLn ("1.- Yes");
+        ioUtils.printOutLn ("0.- No");
+        ioUtils.endLine();
+        ioUtils.printOut("Select option number: ");
+        return Integer.parseInt(ioUtils.input());
+    }
+
+    public String setGameName()
+    {
+        ioUtils.endLine();
+        ioUtils.printOutLn ("Enter a name for the game:");
+        return ioUtils.input();
     }
     /* TESTING METHODS */
 
