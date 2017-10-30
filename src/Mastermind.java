506137d545a6a33348cb4c988f26e9c7290c6c55
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-30 11:21:46 +0100

    Deletes unused drivers & stubs

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,27 +1,10 @@
 import domain.controllers.DomainController;
-import exceptions.IntegrityCorruption;
-import presentation.controllers.PresentationController;
-import testing.MainTestController;
 
 public class Mastermind
 {
-    public static void main(String args[]) throws Exception{
-        if (args.length != 0)
-        {
-            switch (args[0]) {
-                case "-T":
-                case "--TESTING":
-                    MainTestController mainTestController = new MainTestController();
-                    mainTestController.exe();
-                    break;
-                default:
-                    break;
-            }
-        }
-        else
-        {
-            DomainController domainController = new DomainController();
-            domainController.exe();
-        }
+    public static void main(String args[]) throws Exception
+    {
+        DomainController domainController = new DomainController();
+        domainController.exe();
     }
 }
