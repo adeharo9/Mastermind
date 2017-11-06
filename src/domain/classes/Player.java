011b975eebffb3793bb21c4419780b24c84aeea7
RafaelRamirezPozo <rafael.ramirez.pozo@est.fib.upc.edu>
2017-11-06 20:23:55 +0100

    points pasa de Player a Game

--- src/domain/classes/Player.java
+++ src/domain/classes/Player.java
@@ -10,7 +10,7 @@ public abstract class Player implements DeepCopyable, Serializable
     /* ATTRIBUTES */
 
     protected String id;
-    protected int points;
+
 
     /* PRIVATE METHODS */
 
@@ -23,19 +23,16 @@ public abstract class Player implements DeepCopyable, Serializable
     public Player ()
     {
         setId(Utils.autoID());
-        setPoints(0);
     }
 
     public Player(String id) throws IllegalArgumentException
     {
         setId(id);
-        setPoints(0);
     }
 
     public Player (Player player) throws IllegalArgumentException, NullPointerException
     {
         setId(player.getId());
-        setPoints(player.getPoints());
     }
 
     /* SET METHODS */
@@ -48,21 +45,11 @@ public abstract class Player implements DeepCopyable, Serializable
         this.id = id;
     }
 
-    public void setPoints(int points)
-    {
-        this.points = points;
-    }
-
     /* GET METHODS */
 
     public String getId()
     {
         return id;
-    }
-
-    public int getPoints()
-    {
-        return points;
     }
 
     /* TESTING METHODS */
