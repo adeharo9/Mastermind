e344a932d888473ad9f85ed44a91450f5e56b839
Alejandro de Haro <a.deharo9@gmail.com>
2017-11-08 23:59:51 +0100

    Upgrades Color enum to be able to be matched by Integer and by String

--- src/enums/Color.java
+++ src/enums/Color.java
@@ -1,69 +1,82 @@
 package enums;
 
+import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 public enum Color
 {
-    RED,
-    GREEN,
-    BLUE,
-    ORANGE,
-    PURPLE,
-    YELLOW,
-    NONE,
-    BROWN,
-    PINK,
+    RED(0, "R"),
+    GREEN(1, "G"),
+    BLUE(2, "B"),
+    ORANGE(3, "O"),
+    PURPLE(4, "P"),
+    YELLOW(5, "Y"),
+    NONE(6, "N"),
+    BROWN(7, "R"),
+    PINK(8, "I"),
 
-    BLACK,
-    WHITE;
+    BLACK(9, "K"),
+    WHITE(10, "W");
+
+    private int id;
+    private String strId;
+
+    private static HashMap<Integer, Color> int2Color;
+    private static HashMap<String, Color> str2Color;
+
+    static
+    {
+        for(final Color color : Color.values())
+        {
+            int2Color.put(color.id, color);
+            str2Color.put(color.strId, color);
+        }
+    }
+
+    private Color(int id, String strId)
+    {
+        this.id = id;
+        this.strId = strId;
+
+    }
+
+    public static Color getColor(int id) throws IllegalArgumentException
+    {
+        Color color;
+
+        color = int2Color.get(id);
+
+        if(color == null) throw new IllegalArgumentException();
+
+        return color;
+    }
+
+    public static Color getColor(String strId) throws IllegalArgumentException
+    {
+        Color color;
+        color =  str2Color.get(strId);
+
+        if(color == null) throw new IllegalArgumentException();
+
+        return color;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getStrId()
+    {
+        return strId;
+    }
 
     public static Color getRandomColor(int upperBound) throws IllegalArgumentException
     {
         if(upperBound < 1) throw new IllegalArgumentException();
 
-        Color randColor;
-
         int rand = ThreadLocalRandom.current().nextInt(0, upperBound);      // [0, uppedBound)
 
-        switch (rand)
-        {
-            case 0:
-                randColor = RED;
-                break;
-            case 1:
-                randColor = GREEN;
-                break;
-            case 2:
-                randColor = BLUE;
-                break;
-            case 3:
-                randColor = YELLOW;
-                break;
-            case 4:
-                randColor = ORANGE;
-                break;
-            case 5:
-                randColor = PURPLE;
-                break;
-            case 6:
-                randColor = NONE;
-                break;
-            case 7:
-                randColor = BROWN;
-                break;
-            case 8:
-                randColor = WHITE;
-                break;
-            case 9:
-                randColor = BLACK;
-                break;
-            case 10:
-                randColor = PINK;
-                break;
-            default:
-                throw new IllegalArgumentException();
-        }
-
-        return randColor;
+        return Color.getColor(rand);
     }
 }
