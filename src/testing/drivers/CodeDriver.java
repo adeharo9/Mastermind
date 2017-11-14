ebf109413eccf6fd2d30a30786745b521d432b19
Alejandro de Haro <a.deharo9@gmail.com>
2017-11-14 18:41:06 +0100

    Adds driver for code testing

---
+++ src/testing/drivers/CodeDriver.java
@@ -0,0 +1,91 @@
+package testing.drivers;
+
+import domain.classes.Code;
+import enums.Color;
+import enums.Difficulty;
+import util.Constants;
+import util.ioUtils;
+
+import java.util.*;
+
+public class CodeDriver
+{
+    private static final int NUM_TESTS = 1000;
+    private static final Difficulty DIFFICULTY = Difficulty.EASY;
+
+    public CodeDriver()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        CodeDriver codeDriver = new CodeDriver();
+
+        codeDriver.exe();
+    }
+
+    public void exe()
+    {
+        testCodeUnorderedEquals();
+    }
+
+    private void testCodeUnorderedEquals()
+    {
+        int n = NUM_TESTS / 2;
+        List<Code> codeList = new ArrayList<>(n);
+
+        for(int i = 0; i < n; ++i)
+        {
+            Code code = randomCode(DIFFICULTY);
+            codeList.add(code);
+            codeList.add(code);
+        }
+
+        Collections.shuffle(codeList);
+
+        for(int i = 0; i < n; ++i)
+        {
+            Code code1 = codeList.get(i);
+            for(int j = i; j < n; ++j)
+            {
+                Code code2 = codeList.get(j);
+                if(code1.unorderedEquals(code2))
+                {
+                    printCode(code1);
+                    printCode(code2);
+                    ioUtils.endLine();
+                }
+            }
+        }
+    }
+
+    private Code randomCode(Difficulty difficulty)
+    {
+        int numPins = Constants.getNumPinsByDifficulty(difficulty);
+        int numColors = Constants.getNumColorsByDifficulty(difficulty);
+
+        List<Color> colorList = new ArrayList<>(numPins);
+
+        for(int i = 0; i < numPins; ++i)
+        {
+            Color color = Color.getRandomColor(numColors);
+            colorList.add(color);
+        }
+
+        Collections.shuffle(colorList);
+
+        return new Code(colorList);
+    }
+
+    private void printCode(final Code code)
+    {
+        List<Color> colorList = code.getCodePins();
+
+        for(Color color : colorList)
+        {
+            ioUtils.printOut(color.getStrId() + " ");
+        }
+        ioUtils.endLine();
+    }
+}
