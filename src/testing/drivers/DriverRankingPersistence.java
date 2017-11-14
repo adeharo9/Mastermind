dc499c39822894184e4710d44e7de0b1139febbf
Alex Sanchez Gil <alex.sanchez.gil@est.fib.upc.edu>
2017-11-14 22:38:04 +0100

    Driver de la capa de persistencia de ranking acabado.

--- src/testing/drivers/DriverRankingPersistence.java
+++ src/testing/drivers/DriverRankingPersistence.java
@@ -3,11 +3,15 @@ package testing.drivers;
 import domain.classes.Ranking;
 import persistence.AbstractPersistence;
 import persistence.RankingPersistence;
+import util.Pair;
 import util.ioUtils;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class DriverRankingPersistence {
+
+    private Ranking ranking;
 
     public static void main(String[] args){
         DriverRankingPersistence driverRankingPersistence = new DriverRankingPersistence();
@@ -17,6 +21,7 @@ public class DriverRankingPersistence {
     private void exe()
     {
         testSaveRanking();
+        testLoadRanking();
     }
 
     private void testSaveRanking()
@@ -42,15 +47,53 @@ public class DriverRankingPersistence {
         catch (IOException e)
         {
             ioUtils.printOutLn("Test not passed. Error in saveRanking. Possibly because of ranking already exists.");
+            ioUtils.endLine();
         }
 
         if(rankingPersistence.exists("ranking"))
         {
             ioUtils.printOutLn("The ranking has saved correctly!");
+            ioUtils.endLine();
+            this.ranking = ranking;
         }
         else
         {
             ioUtils.printOutLn("Test not passed. Error in saveRanking.");
+            ioUtils.endLine();
         }
+    }
+
+    private void testLoadRanking()
+    {
+        RankingPersistence rankingPersistence = new RankingPersistence();
+        boolean error = false;
+        Ranking rankingLoaded = new Ranking();
+
+        try
+        {
+            rankingLoaded = rankingPersistence.load("ranking");
+        }
+        catch(IOException | ClassNotFoundException e)
+        {
+            error = true;
+        }
+
+        if(!error)
+        {
+            ioUtils.printOutLn("The ranking has loaded correctly!");
+            ioUtils.endLine();
+            
+            LinkedList<Pair<String, Integer>> topTen = rankingLoaded.getTopTen();
+            for(int i = 0; i < topTen.size(); ++i)
+            {
+                ioUtils.printOutLn(Integer.toString(i+1) + ". Player: " + topTen.get(i).first + " Points: " + topTen.get(i).second);
+            }
+            ioUtils.endLine();
+        }
+        else
+        {
+            ioUtils.printOutLn("Test not passed. Error in the load of the ranking!");
+        }
+
     }
 }
