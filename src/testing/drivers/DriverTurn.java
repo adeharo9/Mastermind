169f0670088b976fa80c863f4ac65c781d7aa5c9
RafaelRamirezPozo <rafael.ramirez.pozo@est.fib.upc.edu>
2017-11-16 19:03:45 +0100

    DriverTurn

--- src/testing/drivers/DriverTurn.java
+++ src/testing/drivers/DriverTurn.java
@@ -10,53 +10,12 @@ import java.util.List;
 
 public class DriverTurn {
 
-    private Turn t;
+    private Turn test;
+    private List<Color> colors;
 
-    public Turn testConstructor(){
+    private DriverTurn()
+    {
 
-        ArrayList<Color> c1 = new ArrayList<>();
-        Code c = new Code(c1);
-        Turn t = new Turn(c);
-        t = new Turn(t);
-        ioUtils.printOutLn("Create turn OK");
-        return t;
-    }
-
-    public void testSet(){
-        ArrayList<Color> c1 = new ArrayList<>();
-        c1.add(Color.BLACK);
-        Code c = new Code(c1);
-        t.setCorrection(c);
-        ioUtils.printOutLn("Set turn OK");
-    }
-
-    public void testGetCode(){
-        Code c = t.getCode();
-        ioUtils.printOutLn("Get code OK");
-    }
-
-    public void testGetCorrection(){
-        Code t1 = t.getCorrectionCode();
-        ioUtils.printOutLn("Get correction code OK");
-    }
-
-    public void testGetCorrectionPins(){
-        List<Color> c = t.getCorrectionPins();
-        ioUtils.printOutLn("Get corrections pins OK");
-    }
-
-    public void testGetCorrectionPinAt(){
-        Color c = t.getCorrectionPinAt(0);
-        ioUtils.printOutLn("Get correction color OK");
-    }
-
-    public void exe(){
-        Turn t = testConstructor();
-        /*testSet();*/
-        testGetCode();
-        testGetCorrection();
-        testGetCorrectionPins();
-        /*testGetCorrectionPinAt();*/
     }
 
     public static void main (String args[]){
@@ -66,4 +25,59 @@ public class DriverTurn {
 
     }
 
+    public void exe(){
+        testConstructors();
+        testGetsAndSets();
+    }
+
+    public void testConstructors()
+    {
+        colors = new ArrayList<>();
+        colors.add(Color.BLUE);
+        colors.add(Color.PURPLE);
+        colors.add(Color.GREEN);
+        colors.add(Color.GREEN);
+        Code code = new Code(colors);
+        test = new Turn(code);
+        ioUtils.printOutLn("Success on testConstructors");
+    }
+
+    private void testGetsAndSets()
+    {
+        List<Color> pins = test.getCodePins();
+        boolean error = false;
+
+        for(int i = 0; i < pins.size(); ++i)
+        {
+            if(pins.get(i) != colors.get(i))
+            {
+                ioUtils.printOutLn("Error in getCodePins!");
+                error = true;
+                break;
+            }
+        }
+
+        colors = new ArrayList<>();
+        colors.add(Color.BLACK);
+        colors.add(Color.WHITE);
+        colors.add(Color.NONE);
+        colors.add(Color.NONE);
+        test.setCorrectionPins(colors);
+
+        List<Color> correct = test.getCorrectionPins();
+
+        for(int i = 0; i < correct.size(); ++i)
+        {
+            if(correct.get(i) != colors.get(i))
+            {
+                ioUtils.printOutLn("Error in getCorrectionPins!");
+                error = true;
+                break;
+            }
+        }
+
+        if(!error) ioUtils.printOutLn("Success on testGetsAndSets");
+    }
+
+    
 }
