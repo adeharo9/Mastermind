81f125e02030edc81c8356b35afefbf1e5c3359a
RafaelRamirezPozo <rafael.ramirez.pozo@est.fib.upc.edu>
2017-11-16 22:59:51 +0100

    stubs and driverGameController

--- src/testing/stubs/Game.java
+++ src/testing/stubs/Game.java
@@ -3,6 +3,12 @@ package testing.stubs;
 import java.util.*;
 
 public class Game {
-    public Board getBoard(){return new Board();}
+    private int points;
+    private Board Board;
+
+    public void setPoints(int p){points = p;}
+    public int getPoints(){return points;}
+    public Board getBoard(){return Board;}
+
     public List<Player> getPlayers(){return new ArrayList<>();}
 }
