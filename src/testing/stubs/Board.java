fb855405238e94e7d6a8d879c5a23814057b32e7
RafaelRamirezPozo <rafael.ramirez.pozo@est.fib.upc.edu>
2017-11-16 23:53:05 +0100

    NEEDS REVISION stubs and driverGameController

--- src/testing/stubs/Board.java
+++ src/testing/stubs/Board.java
@@ -3,7 +3,10 @@ package testing.stubs;
 public class Board {
 
     private int currentTurn;
+    private Turn lastTurn;
 
     public void setCurrentTurnNumber(int t){currentTurn = t;}
     public int getCurrentTurnNumber(){return currentTurn;}
+    public Turn getLastTurn(){return lastTurn;}
+
 }
--- src/testing/stubs/Turn.java
+++ src/testing/stubs/Turn.java
@@ -1,4 +1,22 @@
 package testing.stubs;
 
+import enums.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Turn {
+
+    private ArrayList<Color> correctionPins;
+
+    public void setCorrection(Color c){
+        correctionPins.add(c);
+    }
+    public final List<Color> getCorrectionPins()
+    {
+        return correctionPins;
+    }
+    public final Color getCorrectionPinAt(final int i) { return correctionPins.get(i); }
+
+
 }
