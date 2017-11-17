48275d9f076b3a7e67f37dbe3d17d72a7898bdaf
Alex Sanchez Gil <alex.sanchez.gil@est.fib.upc.edu>
2017-11-17 11:52:49 +0100

    Driver Board con interacción.

--- src/testing/drivers/DriverBoard.java
+++ src/testing/drivers/DriverBoard.java
@@ -29,7 +29,25 @@ public class DriverBoard {
     private void testConstructors()
     {
         Board board = new Board();
-        Board boardWithDifficulty = new Board(Difficulty.EASY);
+        Difficulty difficulty = null;
+        ioUtils.printOutLn("Introduce a difficulty (introduce h if you want hard " +
+                "difficulty, m if you want medium and e if you want easy): ");
+        String diff = ioUtils.input();
+        switch(diff) {
+            case "h":
+                difficulty = Difficulty.HARD;
+                break;
+            case "m":
+                difficulty = Difficulty.MEDIUM;
+                break;
+            case "e":
+                difficulty = Difficulty.EASY;
+                break;
+            default:
+                throw new IllegalArgumentException("Error with input");
+        }
+
+        Board boardWithDifficulty = new Board(difficulty);
         this.testedBoard = boardWithDifficulty;
 
         try
@@ -38,34 +56,72 @@ public class DriverBoard {
         }
         catch (NullPointerException e)
         {
-            ioUtils.printOutLn("The constructors functions of sBoard works correctly!\n");
+            ioUtils.printOutLn("The constructors functions of Board works correctly!\n");
         }
     }
 
     private void testGetsAndSets()
     {
-        Difficulty difficulty = testedBoard.getDifficulty();
-        int maxAttempts = testedBoard.getMaxAttempts();
+        Difficulty difficulty = null;
+        ioUtils.printOutLn("Test of getter and setters of Board:");
+        ioUtils.printOutLn("Introduce a difficulty (introduce h if you want hard" +
+                                "difficulty, m if you want medium and e if you want easy");
+
+        String diff = ioUtils.input();
+        switch(diff) {
+            case "h":
+                difficulty = Difficulty.HARD;
+                break;
+            case "m":
+                difficulty = Difficulty.MEDIUM;
+                break;
+            case "e":
+                difficulty = Difficulty.EASY;
+                break;
+            default:
+                throw new IllegalArgumentException("Error with input");
+        }
+
+        testedBoard.setDifficulty(difficulty);
+        Difficulty getDifficulty = testedBoard.getDifficulty();
+        ioUtils.printOutLn("If the setDifficulty and the getDifficulty work correctly, the difficulty introduced has been: " +
+                             getDifficulty.toString());
+
+        ioUtils.printOutLn("Now, introduce a integer that will be the number of maximum attempts:");
+        int maxAttempts = 0;
+        try{
+            maxAttempts = Integer.parseInt(ioUtils.input());
+        }
+        catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+
+        testedBoard.setMaxAttempts(maxAttempts);
+        ioUtils.printOutLn("If the setMaxAttempts and the getMaxAttempts work correctly, the number introduced has been: +" +
+                testedBoard.getMaxAttempts());
+
+        ioUtils.printOutLn("Now, introduce a integer that will be the number of columns:");
+        int nColumns = 0;
+        try{
+            nColumns = Integer.parseInt(ioUtils.input());
+        }
+        catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+
+        testedBoard.setNColumns(nColumns);
+        ioUtils.printOutLn("If the setNColumns and the getNColumns work correctly, the number introduced has been: +" +
+                testedBoard.getNColumns());
+
         int turn = testedBoard.getCurrentTurnNumber();
-        int nColumns = testedBoard.getNColumns();
 
         ioUtils.printOutLn("Difficulty: " + difficulty.toString() + " Max attempts: " + Integer.toString(maxAttempts) +
             " Actual turn: " + Integer.toString(turn) + " Number of columns: " + Integer.toString(nColumns) + "\n");
-
-        testedBoard.setDifficulty(Difficulty.MEDIUM);
-        testedBoard.setMaxAttempts(15);
-        testedBoard.setNColumns(6);
-
-        maxAttempts = testedBoard.getMaxAttempts();
-        turn = testedBoard.getCurrentTurnNumber();
-        nColumns = testedBoard.getNColumns();
-
-        ioUtils.printOutLn("Difficulty: " + difficulty.toString() + " Max attempts: " + Integer.toString(maxAttempts) +
-                " Actual turn: " + Integer.toString(turn) + " Number of columns: " + Integer.toString(nColumns) + "\n");
     }
 
     private void testAddTurn()
     {
+        ioUtils.printOutLn("I");
         List<Color> pins = new ArrayList<>();
         pins.add(Color.BLUE);
         pins.add(Color.ORANGE);
@@ -122,11 +178,20 @@ public class DriverBoard {
 
     private void testAddCodeAndAddCorrection()
     {
+        ioUtils.printOutLn("Test of addCode and addCorrection");
+        ioUtils.printOutLn("Write a code of 4 colors using the following letters (Input example: O Y B G):\n" +
+                "O: Orange\n" +
+                "R: Red\n" +
+                "G: Green\n" +
+                "B: Blue\n" +
+                "Y: Yellow\n" +
+                "P: Purple");
         List<Color> pins = new ArrayList<>();
-        pins.add(Color.BLUE);
-        pins.add(Color.ORANGE);
-        pins.add(Color.PURPLE);
-        pins.add(Color.YELLOW);
+        for(int i = 0; i < 4; ++i)
+        {
+            Color color = Color.getColor(ioUtils.input());
+            pins.add(color);
+        }
 
         Code code = new Code(pins);
         try
@@ -138,8 +203,19 @@ public class DriverBoard {
             ioUtils.printOutLn("Test passed. You can't add a code of 4 pins in a hard board that needs 6!\n");
         }
 
-        pins.add(Color.ORANGE);
-        pins.add(Color.YELLOW);
+        ioUtils.printOutLn("Write a code of 2 colors using the following letters (Input example: O Y):\n" +
+                "O: Orange\n" +
+                "R: Red\n" +
+                "G: Green\n" +
+                "B: Blue\n" +
+                "Y: Yellow\n" +
+                "P: Purple");
+        for(int i = 0; i < 4; ++i)
+        {
+            Color color = Color.getColor(ioUtils.input());
+            pins.add(color);
+        }
+
         code = new Code(pins);
 
         testedBoard.addCode(code);
