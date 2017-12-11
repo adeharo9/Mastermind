13e18e99cbc7a18c991ff3b9c0be1a060f4ae116
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-11 11:37:49 +0100

    Updates main to new logic scheme

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,23 +1,22 @@
+import domain.controllers.DomainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import util.ioUtils;
 
 public class Mastermind extends Application
 {
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("presentation/views/InitSessionView.fxml"));
-        primaryStage.setTitle("Mastermind");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception
+    {
+        DomainController domainController = new DomainController();
+        domainController.exe();
     }
 
     public static void main(String args[]) throws Exception
     {
         launch(args);
-        /*DomainController domainController = new DomainController();
-        domainController.exe();*/
     }
 }
