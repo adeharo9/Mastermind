fd3d0baf9532c30f8734771a3b50be3285ae72fe
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-13 18:37:54 +0100

    Adds dummy LoadingViewController

---
+++ src/presentation/controllers/LoadingViewController.java
@@ -0,0 +1,21 @@
+package presentation.controllers;
+
+public class LoadingViewController extends AbstractViewController
+{
+    public LoadingViewController()
+    {
+
+    }
+
+    @Override
+    protected void initialize()
+    {
+
+    }
+
+    @Override
+    public void start() throws Exception
+    {
+
+    }
+}
