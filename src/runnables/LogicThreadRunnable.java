21f22fc2b470c6c21d01637ff9d0049b9dfa786f
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-13 18:37:40 +0100

    Adds runnable classes for thread execution

---
+++ src/runnables/LogicThreadRunnable.java
@@ -0,0 +1,26 @@
+package runnables;
+
+import domain.controllers.DomainController;
+
+public class LogicThreadRunnable implements Runnable
+{
+    private DomainController domainController;
+
+    public LogicThreadRunnable(DomainController domainController)
+    {
+        this.domainController = domainController;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            domainController.exe();
+        }
+        catch (InterruptedException e)
+        {
+
+        }
+    }
+}
---
+++ src/runnables/UpdateViewRunnable.java
@@ -0,0 +1,32 @@
+package runnables;
+
+import enums.View;
+import presentation.controllers.PresentationController;
+
+import java.io.IOException;
+
+public class UpdateViewRunnable implements Runnable
+{
+
+    private final PresentationController presentationController;
+    private final String viewFile;
+
+    public UpdateViewRunnable(PresentationController presentationController, String viewFile)
+    {
+        this.presentationController = presentationController;
+        this.viewFile = viewFile;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            presentationController.updateView(viewFile);
+        }
+        catch (IOException e)
+        {
+
+        }
+    }
+}
