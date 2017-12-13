e1d6dca73ffea728aea8b37057076d4c00a97329
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-13 18:41:35 +0100

    Adds multithread management

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,17 +1,39 @@
 import domain.controllers.DomainController;
+import enums.View;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import presentation.controllers.PresentationController;
+import runnables.LogicThreadRunnable;
+import util.Constants;
+
+import java.io.IOException;
 
 public class Mastermind extends Application
 {
+    private PresentationController presentationController;
+    private DomainController domainController;
+
     @Override
-    public void start(Stage primaryStage) throws Exception
+    public void init() throws InterruptedException
     {
-        DomainController domainController = new DomainController();
-        domainController.exe();
+        presentationController = new PresentationController();
+        domainController = new DomainController(presentationController);
     }
 
-    public static void main(String args[]) throws Exception
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        presentationController.setStage(stage);
+        presentationController.initView();
+
+        Thread logicThread = new Thread(new LogicThreadRunnable(domainController));
+        logicThread.start();
+    }
+
+    public static void main(String args[])
     {
         launch(args);
     }
