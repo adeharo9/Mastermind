84aeaf8ab87e3bc0955a272cdebdf402332f1201
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-18 14:06:11 +0100

    Promotes password text fields to password fields

--- src/presentation/controllers/RegisterViewController.java
+++ src/presentation/controllers/RegisterViewController.java
@@ -2,6 +2,7 @@ package presentation.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class RegisterViewController extends PresentationController
     /* ATTRIBUTES */
 
     @FXML private TextField usernameTextField;
-    @FXML private TextField passwordTextField;
+    @FXML private PasswordField passwordPasswordField;
 
     /* CONSTRUCTORS */
 
@@ -32,7 +33,7 @@ public class RegisterViewController extends PresentationController
     public void registerButtonAction(ActionEvent actionEvent) throws IOException
     {
         PresentationController.username = usernameTextField.getText();
-        PresentationController.password = passwordTextField.getText();
+        PresentationController.password = passwordPasswordField.getText();
 
         pressButtonAction(1);
     }
