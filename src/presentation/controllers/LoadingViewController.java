b47fbafa8f2d3b8a4fe80dd4b5976411d1873e85
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-18 10:48:30 +0100

    Updates loading screens

--- src/presentation/controllers/LoadingViewController.java
+++ src/presentation/controllers/LoadingViewController.java
@@ -1,9 +1,21 @@
 package presentation.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class LoadingViewController extends PresentationController
 {
     public LoadingViewController()
     {
 
+    }
+
+    /* FXML */
+    @FXML
+    public void closeButtonAction(ActionEvent actionEvent) throws IOException
+    {
+        pressButtonAction(0);
     }
 }
