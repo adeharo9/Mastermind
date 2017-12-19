775d5592d5591bcbf94bb28a8496c859d7f9a78c
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-19 23:42:15 +0100

    Adds popup controller abstraction layer

---
+++ src/presentation/controllers/CloseProgramWarningViewController.java
@@ -0,0 +1,35 @@
+package presentation.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CloseProgramWarningViewController extends PopUpController
+{
+    public CloseProgramWarningViewController()
+    {
+
+    }
+
+    protected void pressButtonSpecificAction()
+    {
+        this.popUpStage.close();
+    }
+
+    /* FXML */
+
+    @FXML
+    public void exitButtonAction(ActionEvent actionEvent) throws IOException
+    {
+        super.yesButtonAction(actionEvent);
+    }
+
+    @FXML
+    public void cancelButtonAction(ActionEvent actionEvent) throws IOException
+    {
+        super.noButtonAction(actionEvent);
+    }
+}
--- src/presentation/controllers/PopUpController.java
+++ src/presentation/controllers/PopUpController.java
@@ -1,35 +1,19 @@
 package presentation.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class WarningPopUpController extends PresentationController
+public abstract class PopUpController extends PresentationController
 {
-    public WarningPopUpController()
-    {
-
-    }
-
-    protected void pressButtonSpecificAction()
-    {
-        this.popUpStage.close();
-    }
-
-    /* FXML */
-
-    @FXML
     public void yesButtonAction(ActionEvent actionEvent) throws IOException
     {
         pressButtonAction(1);
     }
 
-    @FXML
     public void noButtonAction(ActionEvent actionEvent) throws IOException
     {
         pressButtonAction(0);
     }
+
 }
