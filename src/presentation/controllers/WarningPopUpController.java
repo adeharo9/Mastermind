f820c89cee1c4a31011a1efd571a29d78b26a4fc
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-19 23:27:12 +0100

    Adds popup view support

---
+++ src/presentation/controllers/WarningPopUpController.java
@@ -0,0 +1,37 @@
+package presentation.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WarningPopUpController extends PresentationController
+{
+    @FXML
+    private Button yesButton;
+    public WarningPopUpController()
+    {
+
+    }
+
+    protected void pressButtonSpecificAction()
+    {
+        this.popUpStage.close();
+    }
+
+    /* FXML */
+
+    @FXML
+    public void yesButtonAction(ActionEvent actionEvent) throws IOException
+    {
+        pressButtonAction(1);
+    }
+
+    @FXML
+    public void noButtonAction(ActionEvent actionEvent) throws IOException
+    {
+        pressButtonAction(0);
+    }
+}
---
+++ src/runnables/PopUpViewRunnable.java
@@ -0,0 +1,31 @@
+package runnables;
+
+import presentation.controllers.PresentationController;
+
+import java.io.IOException;
+
+public class PopUpViewRunnable implements Runnable
+{
+
+    private final PresentationController presentationController;
+    private final String viewFile;
+
+    public PopUpViewRunnable(PresentationController presentationController, String viewFile)
+    {
+        this.presentationController = presentationController;
+        this.viewFile = viewFile;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            presentationController.popUpWindow(viewFile);
+        }
+        catch (IOException e)
+        {
+
+        }
+    }
+}
