4bf8188547ca98ac39b5e483ebd6fb4457ba7013
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-19 15:42:10 +0100

    Adds game in progress controller

---
+++ src/presentation/controllers/GameInProgressViewController.java
@@ -0,0 +1,45 @@
+package presentation.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+
+import java.util.ArrayList;
+
+public class GameInProgressViewController extends PresentationController
+{
+    /* ATTRIBUTES */
+
+    @FXML private Circle bigPinTemplate;
+    @FXML private Circle smallPinTemplate;
+    @FXML private HBox rowTemplate;
+    @FXML private VBox board;
+
+    private ArrayList<Circle> colorSelectors;
+
+    /* CONSTRUCTORS */
+
+    public GameInProgressViewController()
+    {
+        colorSelectors = new ArrayList<>();
+    }
+
+    public void printColorSelectors()
+    {
+
+    }
+
+    public void printBoard()
+    {
+
+    }
+
+    /* FXML */
+
+    @FXML
+    public void initialize()
+    {
+        colorSelectors.add(bigPinTemplate);
+    }
+}
