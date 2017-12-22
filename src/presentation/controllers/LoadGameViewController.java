758b428c68a9252951d3e6b8894ed0015c60fb9b
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-22 09:43:34 +0100

    Adds loading game view

---
+++ src/presentation/controllers/LoadGameViewController.java
@@ -0,0 +1,47 @@
+package presentation.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class LoadGameViewController extends PresentationController
+{
+    @FXML private Label errorLabel;
+    @FXML private ScrollPane contentScrollPane;
+
+    public LoadGameViewController()
+    {
+
+    }
+
+    public void errorMessage(String message)
+    {
+        errorLabel.setVisible(true);
+        errorLabel.setText(message);
+    }
+
+    /* FXML */
+
+    @FXML
+    public void initialize()
+    {
+        contentScrollPane.setFitToHeight(true);
+        contentScrollPane.setFitToWidth(true);
+    }
+
+    @FXML
+    public void backButtonAction(ActionEvent actionEvent) throws IOException
+    {
+        pressButtonAction(0);
+    }
+
+    @FXML
+    public void editButtonAction(ActionEvent actionEvent) throws IOException
+    {
+        pressButtonAction(1);
+    }
+}
