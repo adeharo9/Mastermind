00bddabf44de672cdbafbaf386c6c8d8e72330e5
Alejandro de Haro <a.deharo9@gmail.com>
2017-12-30 19:08:59 +0100

    Adds show code view for showing code on code maker game mode

---
+++ src/presentation/controllers/ShowCodeViewController.java
@@ -0,0 +1,44 @@
+package presentation.controllers;
+
+import enums.Color;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+
+public class ShowCodeViewController extends NonRegisteringPresentationController
+{
+    /* ATTRIBUTES */
+
+    @FXML private HBox codeHBox;
+
+    public ShowCodeViewController()
+    {
+
+    }
+
+    /* FXML */
+
+    @FXML
+    public void initialize()
+    {
+        int column = 0;
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setAlignment(Pos.CENTER);
+
+        for(final Color color : solution)
+        {
+            Node pin = getNewPin(color);
+
+            gridPane.add(pin, column, 0);
+
+            ++column;
+        }
+
+        codeHBox.getChildren().setAll(gridPane);
+    }
+}
