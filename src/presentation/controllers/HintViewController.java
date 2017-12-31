c395333f16d4a9e33c9f8c65724ea6161d2d759c
RafaelRamirezPozo <rafael.ramirez.pozo@est.fib.upc.edu>
2017-12-31 15:25:52 +0100

    hintview

--- src/presentation/controllers/HintViewController.java
+++ src/presentation/controllers/HintViewController.java
@@ -16,21 +16,6 @@ public class HintViewController extends PopUpController
 
     }
 
-    /*public void showClue(int type, String number, String color) throws IllegalArgumentException
-    {
-        switch(type)
-        {
-            case 1:
-                hintLabel.setText("Token in position " + number + " is " + color + ".");
-                break;
-            case 2:
-                hintLabel.setText("There is/are " + number + " " + color + " tokens" + ".");
-                break;
-            default:
-                throw new IllegalArgumentException();
-        }
-    }*/
-
     protected void pressButtonTemplateAction()
     {
         this.popUpStage.close();
@@ -41,7 +26,7 @@ public class HintViewController extends PopUpController
     @FXML
     public void okButtonAction() throws IOException
     {
-
+        super.noButtonAction();
     }
 
 }
