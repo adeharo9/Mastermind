6488a9ea2972deff924eaa59f0baf48c51d8ad0d
Alejandro de Haro <a.deharo9@gmail.com>
2018-01-02 20:08:39 +0100

    Introduces popup styles

---
+++ src/enums/PopUpWindowStyle.java
@@ -0,0 +1,41 @@
+package enums;
+
+import javafx.event.EventHandler;
+import javafx.stage.Modality;
+import javafx.stage.WindowEvent;
+import util.Constants;
+
+import java.io.IOException;
+
+public enum PopUpWindowStyle
+{
+    INFO ("Mastermind", Constants.ICON_FILE, Modality.NONE),
+    INTERACTION("Mastermind", Constants.ICON_FILE, Modality.APPLICATION_MODAL),
+    WARNING ("Warning", Constants.WARNING_ICON_FILE, Modality.APPLICATION_MODAL);
+
+    private final String title;
+    private final String iconFile;
+    private final Modality modality;
+
+    private PopUpWindowStyle(final String title, final String iconFile, final Modality modality)
+    {
+        this.title = title;
+        this.iconFile = iconFile;
+        this.modality = modality;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getIconFile()
+    {
+        return iconFile;
+    }
+
+    public Modality getModality()
+    {
+        return modality;
+    }
+}
