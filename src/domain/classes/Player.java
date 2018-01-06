e1410484181dd09b9ab837327eb52f110fed6b9e
Alex Sanchez Gil <alex.sanchez.gil@est.fib.upc.edu>
2018-01-06 18:37:07 +0100

    javadoc Player

--- src/domain/classes/Player.java
+++ src/domain/classes/Player.java
@@ -59,11 +59,23 @@ public abstract class Player implements DeepCopyable, Serializable
 
     /* GET METHODS */
 
+    /**
+     * Getter del id.
+     *
+     * Devuelve el identificador del jugador.
+     *
+     */
     public final String getId()
     {
         return id;
     }
 
+    /**
+     * Getter del rol.
+     *
+     * Devuelve el rol del jugador.
+     *
+     */
     public final Role getRole()
     {
         return role;
@@ -71,11 +83,26 @@ public abstract class Player implements DeepCopyable, Serializable
 
     /* SET METHODS */
 
+    /**
+     * Setter del id.
+     *
+     * Establece el identificador del jugador.
+     *
+     * @param id Identificador del jugador.
+     */
     public void setId(String id)
     {
         this.id = id;
     }
 
+    /**
+     * Setter del rol.
+     *
+     * Establece el rol del jugador.
+     *
+     * @param role Rol del jugador.
+     * @throws IllegalArgumentException Si el rol es null.
+     */
     public void setRole(Role role) throws IllegalArgumentException
     {
         boolean b = role != null;
@@ -84,15 +111,41 @@ public abstract class Player implements DeepCopyable, Serializable
         this.role = role;
     }
 
+    /**
+     * Setter de la contraseña.
+     *
+     * Establece la contraseña del jugador en caso
+     * que este sea de tipo Human.
+     *
+     * @param password Contraseña del jugador.
+     * @throws IllegalArgumentException Si la contraseña no es válida.
+     * @throws NullPointerException Si la contraseña es null.
+     */
     public void setPassword(final String password) throws IllegalArgumentException, NullPointerException {}
 
     /* VALIDATION METHODS */
 
+    /**
+     * Validar id.
+     *
+     * Comprueba que el id del jugador es válido.
+     *
+     * @return boolean.
+     */
     public boolean isValid()
     {
         return isValidId(id);
     }
 
+    /**
+     * Comprobar contraseña.
+     *
+     * Comprueba que la contraseña password se
+     * corresponde a la del jugador.
+     *
+     * @param password Contraseña a comprobar.
+     * @return boolean.
+     */
     public boolean checkPassword(final String password)
     {
         return true;
@@ -105,9 +158,40 @@ public abstract class Player implements DeepCopyable, Serializable
 
     /* PLAYING METHODS */
 
+    /**
+     * Acción como code maker.
+     *
+     * Permite al jugador realizar una acción como code maker.
+     *
+     * @param difficulty Dificultad de la partida.
+     * @throws ReservedKeywordException Si se espera un tipo objeto pero se pretende ir hacia atrás.
+     * @throws InterruptedException Si se interrumpe la ejecución.
+     */
     public abstract Action codeMake(final Difficulty difficulty) throws ReservedKeywordException, InterruptedException;
 
+    /**
+     * Acción como code breaker.
+     *
+     * Permite al jugador realizar una acción como code breaker.
+     *
+     * @param difficulty Dificultad de la partida.
+     * @param lastTurn Último turno de la partida.
+     * @param isFirstTurn Indica si se trata del primer turno.
+     * @throws ReservedKeywordException Si se espera un tipo objeto pero se pretende ir hacia atrás.
+     * @throws InterruptedException Si se interrumpe la ejecución.
+     */
     public abstract Action codeBreak(final Difficulty difficulty, final Turn lastTurn, final boolean isFirstTurn) throws ReservedKeywordException, InterruptedException;
 
+    /**
+     * Acción como code correct.
+     *
+     * Permite al jugador corregir la acción de otro jugador.
+     *
+     * @param difficulty Dificultad de la partida.
+     * @param code Código introducido a corregir.
+     * @param solution Código de colores que representa la solución.
+     * @throws ReservedKeywordException Si se espera un tipo objeto pero se pretende ir hacia atrás.
+     * @throws InterruptedException Si se interrumpe la ejecución.
+     */
     public abstract Action codeCorrect(final Difficulty difficulty, final Code code, final Code solution) throws ReservedKeywordException, InterruptedException;
 }
