5d757271c31ded5f70022329ea24f945421ab54d
Alejandro de Haro <a.deharo9@gmail.com>
2018-01-06 21:37:46 +0100

    Updates javadoc. Adds runtime exceptions

--- src/presentation/runnables/PopUpViewRunnable.java
+++ src/presentation/runnables/PopUpViewRunnable.java
@@ -5,12 +5,37 @@ import presentation.controllers.PresentationController;
 
 import java.io.IOException;
 
+/**
+ * Clase ejecutable de creación de nueva ventana de popup.
+ *
+ * Clase encargada de encapsular el código a ser ejecutado desde
+ * un thread paralelo al de la la lógica del programa con la finalidad
+ * de abrir una nueva ventana de popup en la interfaz gráfica.
+ *
+ * @author Alejandro de Haro
+ */
+
 public class PopUpViewRunnable implements Runnable
 {
     private final PresentationController presentationController;
     private final PopUpWindowStyle popUpWindowStyle;
     private final String viewFile;
 
+    /**
+     * Constructora por defecto.
+     *
+     * Constructora de una instancia de ejecutable con los
+     * distintos parámetros necesarios para abrir una nueva
+     * ventana de popup.
+     *
+     * @param presentationController Controlador de presentación a utilizar
+     *                               para gestionar la creación y apertura de
+     *                               la nueva ventana.
+     * @param popUpWindowStyle Enumeración que indica el tipo de plantilla predefinida
+     *                         de estilo a utilizar en la generación de la nueva ventana
+     *                         de popup.
+     * @param viewFile Nombre del archivo de vista a cargar en la nueva ventana de popup.
+     */
     public PopUpViewRunnable(final PresentationController presentationController, final PopUpWindowStyle popUpWindowStyle, final String viewFile)
     {
         this.presentationController = presentationController;
@@ -18,6 +43,11 @@ public class PopUpViewRunnable implements Runnable
         this.viewFile = viewFile;
     }
 
+    /**
+     * Función run.
+     *
+     * Ejecuta el código indicado cuando es llamado por el nuevo thread.
+     */
     @Override
     public void run()
     {
