cf1f390944420feeb418208216ea88d18794eb23
Alejandro de Haro <a.deharo9@gmail.com>
2018-01-06 20:25:12 +0100

    Starts adding javadoc

--- src/presentation/runnables/LogicThreadRunnable.java
+++ src/presentation/runnables/LogicThreadRunnable.java
@@ -2,6 +2,13 @@ package presentation.runnables;
 
 import domain.controllers.DomainController;
 
+/**
+ * Clase ejecutable de thread lógico
+ *
+ * Clase encargada de encapsular el código a ser ejecutado desde
+ * un thread paralelo al de la interfaz gráfica, para evitar que
+ * en el caso de un
+ */
 public class LogicThreadRunnable implements Runnable
 {
     private DomainController domainController;
