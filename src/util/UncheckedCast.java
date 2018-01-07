7480039e84ad7822a549f42f8664556d364e5933
Alejandro de Haro <a.deharo9@gmail.com>
2018-01-07 04:32:26 +0100

    Some utils javadoc

--- src/util/UncheckedCast.java
+++ src/util/UncheckedCast.java
@@ -1,7 +1,24 @@
 package util;
 
+/**
+ * Clase de casteo sin comprobación
+ *
+ * Silencia los avisos del compilador y los concentra en un único punto del código,
+ * aumentando la explicidad con que se realizan estos casteos.
+ */
+
 public abstract class UncheckedCast
 {
+    /**
+     * Casteo sin comprobación
+     *
+     * Silencia los avisos del compilador y los concentra en un único punto del código,
+     * aumentando la explicidad con que se realizan estos casteos.
+     *
+     * @param object Objeto origen
+     * @param <T> Tipo objetivo
+     * @return Objeto casteado a tipo T
+     */
     @SuppressWarnings("unchecked")
     public static <T> T cast(final Object object)
     {
